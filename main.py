d72f7682e51ba26fc435ba17a29971eb535f5e05
lafoxo <info4lolo6@gmail.com>
2024-12-19 20:24:37 +0100

    commit

---
+++ main.py
@@ -0,0 +1,10 @@
+tree1 = 95
+tree2 = 94
+tree3 = 93
+tree4 = 92
+tree5 = 91
+sum =tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 treee is:",sum)
+
+average=sum/5
+print("the average of all the tree is :",average)
